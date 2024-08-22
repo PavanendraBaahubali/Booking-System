9b33a6c4a97082d466db7a002f628d64d6156cfa
Pavan Kumar <konapala.pawan.kumar@gmail.com>
2024-08-22 16:30:15 +0530

    Just removed no args contructor.

--- src/main/java/com/Movie/Movie/Booking/System/DTO/TheatreDTOs/TheatreDTO.java
+++ src/main/java/com/Movie/Movie/Booking/System/DTO/TheatreDTOs/TheatreDTO.java
@@ -2,15 +2,14 @@ package com.Movie.Movie.Booking.System.DTO.TheatreDTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class TheatreDTO {
     private Long theatreId;
     private String theatreName;
-
-    public TheatreDTO() {
-    }
 }
